44294f72b600530032ed5e395876a23609800da3
Balakumar <bala93kumar@gmail.com>
2022-04-30 22:55:07 +0530

    example Application

---
+++ example6/src/main/java/com/balakumar/eazyschool/Example6Application.java
@@ -0,0 +1,15 @@
+package com.balakumar.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.balakumar.eazyschool.controller")
+public class Example6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example6Application.class, args);
+	}
+
+}
---
+++ example6/src/main/java/com/balakumar/eazyschool/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.balakumar.eazyschool.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public  String displayName() {
+        return "welcome to bala Academy";
+    }
+}
