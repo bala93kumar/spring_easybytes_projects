0e67e6aa90734a53c3181192667b111bded97afc
Balakumar <bala93kumar@gmail.com>
2022-05-03 10:51:52 +0530

    hello app

--- example7/src/main/java/com/balakumar/eazyschool/controller/HomeController.java
+++ example7/src/main/java/com/balakumar/eazyschool/controller/HomeController.java
@@ -2,14 +2,17 @@ package com.balakumar.eazyschool.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping(value={"","/","home"})
-    public  String displayName() {
-        return "welcome to bala Academy";
+    public  String displayName(Model model)
+    {
+        model.addAttribute("username", "Krithiga Balakumar");
+        return  "home.html";
     }
 }
